a57f463e106878af4e7b6f2146ae61c9c227e52b
jaumard <jimmy.aumard@gmail.com>
2019-09-02 10:24:35 +0200

    fix black screen on android in profile/release mode in some devices

--- lib/flutter_camera_ml_vision.dart
+++ lib/flutter_camera_ml_vision.dart
@@ -169,8 +169,7 @@ class CameraMlVisionState<T> extends State<CameraMlVision<T>> {
     }
     _cameraController = CameraController(
       description,
-      widget.resolution ??
-          (Platform.isIOS ? ResolutionPreset.low : ResolutionPreset.medium),
+      widget.resolution ?? ResolutionPreset.low, // As the doc says, better to set low when streaming images to avoid drop frames on older devices
       enableAudio: false,
     );
     if (!mounted) {
@@ -196,6 +195,7 @@ class CameraMlVisionState<T> extends State<CameraMlVision<T>> {
       description.sensorOrientation,
     );
 
+    await Future.delayed(Duration(milliseconds: 200));//hacky technique to avoid having black screen on some android devices
     start();
   }
 
